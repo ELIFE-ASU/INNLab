8fb79e97182344fdb635e76e8ff03cfa99815c48
Zhangyanbo <Zhang.Yanbo@asu.edu>
2021-04-27 23:07:33 -0600

    update version infomation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="INNLab",
-    version="0.1.0",
+    version="0.2.0",
     author="Yanbo Zhang",
     author_email="zhangybspm@gmail.com",
     description="A package for invertible neural networks",
